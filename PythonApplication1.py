dc4f389c524d58d6e60b1d8da0f52800441c0cb9
Opilane TTHK <opilane@tthk.ee>
2021-11-10 11:18:38 +0200

    Add project files.

---
+++ PythonApplication1.py
@@ -0,0 +1,6 @@
+while True:
+    print("funktsioonid".center(50,"+"))
+    v=input("aritmetic- A")
+    if v.upper()=="A":
+        aritmetic()
+
---
+++ module1.py
@@ -0,0 +1,11 @@
+def aritmetic(a: float,b:float,c:str):
+    """lihtne kalkultor
+    +-liitmine
+    --lahutamine
+    *-korrutamine
+    /-jagamine
+    :param float a: Esimine arv
+    :param float b: teine arv
+    :param str c: aritmetiline ´tehig
+    :rtype float:
+    """
