bba65621d9065e5a53c85b932417a4ed77812331
opilane <opilane@HP-CZC6297BBB>
2021-11-10 12:23:59 +0200

    eto o4en slozno

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,6 +1,15 @@
+from module1 import *
 while True:
-    print("funktsioonid".center(50,"+"))
-    v=input("aritmetic- A")
+    print("funksioind".center(50,"+"))
+    print("arithmetic- A,\nis_year_leap-Y")
+    v=input("arithmetic- A")
     if v.upper()=="A":
-        aritmetic()
-
+        a=float(input("esimene arv"))
+        b=float(input("teine arv"))
+        sym=input("Tehe:")
+        rezult=arithmetic(a,b,sym)
+        print(rezult)
+    elif v.upper()=="Y":
+        is_year_leap()
+        rezolut=is_year_leap((int(input("sisesta aasta"))))
+ 
--- module1.py
+++ module1.py
@@ -1,11 +1,41 @@
-def aritmetic(a: float,b:float,c:str):
-    """lihtne kalkultor
+def arithmetic(a: float,b:float,c:str):
+    """Lihtne kalkulaator
     +-liitmine
     --lahutamine
     *-korrutamine
     /-jagamine
-    :param float a: Esimine arv
-    :param float b: teine arv
-    :param str c: aritmetiline ´tehig
-    :rtype float:
+    :param float a: Esimene arv
+    :param float b: Teine arv
+    :param str c: Arimeetiline tehing
+    :rtype float:"""
+    if c=="+":
+        r=a+b
+    elif c=="-":
+        r=a-b
+    elif c=="*":
+        r=a*b
+    elif c==":":
+        if b!=0:
+            r=a/b
+        else:
+            print("Div0")
+            r=0.0
+    else:
+        print("viga")
+    print(r)
+    return r
+def is_year_leap(aasta: int):
+    """Liigaasta leidmine
+    Tagastab true kui aasta on liigasta ja Flase kui ei ole 
+    :parem int aasta: Aasata number 
+    :rtype bool: Funksionid vastus loogilised formaadis
+    """
+    if aasta%4==0:
+        vastus=True
+    else:
+        vastus=False
+    return vstus
+def square():
+    """
+    see arvutab ruudu ümbermõõdu, ruudu pindala ja ruudu diagonaali.
     """
