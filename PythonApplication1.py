b171976e1e713cdf323f905e46683123259f357d
opilane <opilane@HP-CZC6297BBB>
2021-11-12 11:48:28 +0200

    фф

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,7 +1,7 @@
 from module1 import *
 while True:
     print("Funktsioonid".center(50,"+"))
-    v=input("arithmetic- A, \nis_year_leap- B, \nsquare- C, \nseason- D, \nbank- E ,\nkodeeriumine_de- X")
+    v=input("arithmetic- A \nis_year_leap- B \nsquare- C \nseason- D \nbank- E \nis_prime- F \ndate- G \nxor_cipher- K")
     if v.upper()=="A":
         a=float(input("Arv 1: "))
         b=float(input("Arv 2: "))
@@ -29,11 +29,16 @@ while True:
         a=int(input("Sisestage number: "))
         result=is_prime(a)
         print(result)
-    elif v.upper()=="X":
-        print("kodeeriumine".center(60,"*"))
-        rezult=xor_cipher(input("siseta text"), input("sisesta võti :"))
-        print(rezult)
-        print("Dekoderiumine". center(60,"*"))
-        de_rezult=xor_uncipher(rezult, input("sisesta võti:"))
+    elif v.upper()=="K":
+        print("Kodeerimine".center(60,"*"))
+        result=xor_cipher(input("Sisesta text"), input("Sisesta võti :"))
+        print(result)
+        print("Dekodeerimine". center(60,"*"))
+        de_rezult=xor_uncipher(result, input("Sisesta võti:"))
         print(de_rezult)
- 
+    elif v.upper()=="G":
+        day=int(input("Sisestage päev: "))
+        month=int(input("Sisestage kuu: "))
+        year=int(input("Sisestage aasta: "))
+        result=date(day,month,year)
+        print(result)
--- module1.py
+++ module1.py
@@ -8,7 +8,7 @@ def arithmetic(a: float,b:float,c=str):
     :param float b: Teine arv
     :param str c: Aritmeetiline tehing
     :rtype float:
-    """
+   """
     if c=="+":
         r=a+b
     elif c=="-":
@@ -24,15 +24,21 @@ def arithmetic(a: float,b:float,c=str):
         print("Viga!")
     return("")
 def is_year_leap(year:int):
+    """Мы пишем произвольный год, и программа решает, является ли виза на год правильной или неправильной.
+    """
     if year%4==0:
         print("True")
     else:
         print("False")
     return("")
 def square(kv:float):
+    """Мы пишем сторону квадрата, и программа выдает нам площадь квадрата, периметр и диагональ
+    """
     return(4*kv, kv**2, (2*kv**2)**.5)
     return("")
 def season(kuu:int):
+    """Мы пишем от 1 до 12 месяцев, и программа устанавливает сезон по месяцам
+    """
     if kuu==12:
         print("Зима")
     elif 0<kuu<3:
@@ -47,28 +53,32 @@ def season(kuu:int):
         print("Viga!")
     return
 def bank(a:float,years:int):
+    """Мы кладем деньги на баланс и ждем n лет
+    """
     for _ in range(years):
         a=((1.1*1/100)*a)*100
     print("Ваш баланс:",a)
     return("")
 def is_prime(a:int):
+    """Мы записываем число от 0 до 1000 и возвращаем true, если оно простое, и false в противном случае.
+    """
     b=2
     while a%b!=0:
         b+=1
     return b==a
 def xor_cipher(string:str, key:str)->str:
-    #tavaline sõna kodeeristakse
-    
-     result=""
-     temp=int()
-     for i in range(len(string)):
-         temp=ord(string[i])
-         for j in range(len(key)):
-             temp^=ord(key[j])
-         result+=chr(temp)
-     return result
+    """Обычное кодирование слов
+    """
+    result=""
+    temp=int()
+    for i in range(len(string)):
+        temp=ord(string[i])
+        for j in range(len(key)):
+            temp^=ord(key[j])
+        result+=chr(temp)
+    return result
 def xor_uncipher(string:str, key: str)->str:
-    """koderiumine text dekodeeritakse
+    """кодирование декодированного текста
     """
     result = ""
     temp = []
@@ -78,3 +88,12 @@ def xor_uncipher(string:str, key: str)->str:
             temp[i] = chr(ord(key[j]) ^ ord(temp[i]))
         result += temp[i]
     return result
+def date(day:int, month:int, year:int):
+    set_months = {1: 31,2: 28, 3: 31,4: 30,5: 31,6: 30,7: 31,8: 31,9: 30,10: 31,11: 30,12: 31}
+    if year>0 and (month>=1 and month<=12):
+        if day in range(1, set_months[month]+1):
+           return True
+        else:
+            return False
+    else:
+        return False
