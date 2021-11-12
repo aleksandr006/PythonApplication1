59baa70c4ab6f4c77b403f25feeccfc66b9d7bc4
opilane <opilane@HP-CZC6297BBB>
2021-11-12 11:15:48 +0200

    aa

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,15 +1,39 @@
 from module1 import *
 while True:
-    print("funksioind".center(50,"+"))
-    print("arithmetic- A,\nis_year_leap-Y")
-    v=input("arithmetic- A")
+    print("Funktsioonid".center(50,"+"))
+    v=input("arithmetic- A, \nis_year_leap- B, \nsquare- C, \nseason- D, \nbank- E ,\nkodeeriumine_de- X")
     if v.upper()=="A":
-        a=float(input("esimene arv"))
-        b=float(input("teine arv"))
-        sym=input("Tehe:")
-        rezult=arithmetic(a,b,sym)
+        a=float(input("Arv 1: "))
+        b=float(input("Arv 2: "))
+        c=input("Tehe: ")
+        result=arithmetic(a,b,c)
+        print(result)
+    elif v.upper()=="B":
+        year=int(input("Sisesta aasta: "))
+        result=is_year_leap(year)
+        print(result)
+    elif v.upper()=="C":
+        kv=int(input("Sisestage ruudu külg: "))
+        result=square(kv)
+        print(result)
+    elif v.upper()=="D":
+        kuu=int(input("Sisestage kuu number: "))
+        result=season(kuu)
+        print(result)
+    elif v.upper()=="E":
+        a=float(input("Sisestage sissemakse summa: "))
+        years=int(input("Mitu aastat on möödunud?"))
+        result=bank(a,years)
+        print(result)
+    elif v.upper()=="F":
+        a=int(input("Sisestage number: "))
+        result=is_prime(a)
+        print(result)
+    elif v.upper()=="X":
+        print("kodeeriumine".center(60,"*"))
+        rezult=xor_cipher(input("siseta text"), input("sisesta võti :"))
         print(rezult)
-    elif v.upper()=="Y":
-        is_year_leap()
-        rezolut=is_year_leap((int(input("sisesta aasta"))))
+        print("Dekoderiumine". center(60,"*"))
+        de_rezult=xor_uncipher(rezult, input("sisesta võti:"))
+        print(de_rezult)
  
--- module1.py
+++ module1.py
@@ -1,41 +1,80 @@
-def arithmetic(a: float,b:float,c:str):
+def arithmetic(a: float,b:float,c=str):
     """Lihtne kalkulaator
-    +-liitmine
-    --lahutamine
-    *-korrutamine
-    /-jagamine
+    + - liitamine
+    - - lahutamine
+    * - korrutamine
+    / - jagamine
     :param float a: Esimene arv
     :param float b: Teine arv
-    :param str c: Arimeetiline tehing
-    :rtype float:"""
+    :param str c: Aritmeetiline tehing
+    :rtype float:
+    """
     if c=="+":
         r=a+b
     elif c=="-":
         r=a-b
     elif c=="*":
         r=a*b
-    elif c==":":
+    elif c=="/":
         if b!=0:
             r=a/b
         else:
             print("Div0")
-            r=0.0
     else:
-        print("viga")
-    print(r)
-    return r
-def is_year_leap(aasta: int):
-    """Liigaasta leidmine
-    Tagastab true kui aasta on liigasta ja Flase kui ei ole 
-    :parem int aasta: Aasata number 
-    :rtype bool: Funksionid vastus loogilised formaadis
-    """
-    if aasta%4==0:
-        vastus=True
+        print("Viga!")
+    return("")
+def is_year_leap(year:int):
+    if year%4==0:
+        print("True")
     else:
-        vastus=False
-    return vstus
-def square():
+        print("False")
+    return("")
+def square(kv:float):
+    return(4*kv, kv**2, (2*kv**2)**.5)
+    return("")
+def season(kuu:int):
+    if kuu==12:
+        print("Зима")
+    elif 0<kuu<3:
+        print("Зима")
+    elif 2<kuu<6:
+        print("Весна")
+    elif 5<kuu<9:
+        print("Лето")
+    elif 8<kuu<12:
+        print("Осень")
+    else:
+        print("Viga!")
+    return
+def bank(a:float,years:int):
+    for _ in range(years):
+        a=((1.1*1/100)*a)*100
+    print("Ваш баланс:",a)
+    return("")
+def is_prime(a:int):
+    b=2
+    while a%b!=0:
+        b+=1
+    return b==a
+def xor_cipher(string:str, key:str)->str:
+    #tavaline sõna kodeeristakse
+    
+     result=""
+     temp=int()
+     for i in range(len(string)):
+         temp=ord(string[i])
+         for j in range(len(key)):
+             temp^=ord(key[j])
+         result+=chr(temp)
+     return result
+def xor_uncipher(string:str, key: str)->str:
+    """koderiumine text dekodeeritakse
     """
-    see arvutab ruudu ümbermõõdu, ruudu pindala ja ruudu diagonaali.
-    """
+    result = ""
+    temp = []
+    for i in range(len(string)):
+        temp.append(string[i])
+        for j in reversed(range(len(key))):
+            temp[i] = chr(ord(key[j]) ^ ord(temp[i]))
+        result += temp[i]
+    return result
